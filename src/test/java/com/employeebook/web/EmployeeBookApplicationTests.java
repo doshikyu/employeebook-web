f2bd73b55b67d6fcc23290bdb4493b94a1cc4f8a
doshikyu <yu.dashik@gmail.com>
2024-09-03 00:03:50 +0900

    Initial Commit

---
+++ src/test/java/com/employeebook/web/EmployeeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeebook.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
